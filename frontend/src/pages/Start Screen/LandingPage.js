029a7a2c124e7e53c5fc3811c7ed528769f7f448
Adam Gambari <adamgambari@outlook.com>
2024-06-25 02:35:58 -0400

    Finally almost done the landing page!

--- frontend/src/pages/Start Screen/LandingPage.js
+++ frontend/src/pages/Start Screen/LandingPage.js
@@ -14,26 +14,42 @@ const LandingPage = () => {
           <li><a href="#signup">Sign Up</a></li>
         </ul>
       </nav>
+
+      <div className="kaaba" id="kaaba">
+        <div className="kaaba">
+          <img src="muslim-pilgrimage.png" alt="kaaba" />
+          </div>
+        </div>
+
+        <div className="guide-pan">
+          <img src="guide-panflip.png" alt="Guidance" />
+        </div>
+
+        <div className="guide-mark" id="guide-mark">
+        <div className="guide-mark">
+          <img src="guide-mark.png" alt="kaaba" />
+          </div>
+        </div>
+
       <div className="header" id="home">
-        <img src="muslim-pilgrimage.png" alt="Kaaba" className="kaaba-image" />
         <h1>Pilgrimage</h1>
         <p>Community-Verified Umrah Planning with Real-Time Guidance</p>
         <button className="sign-up-button">Sign Up Here!</button>
       </div>
-      <div className="features" id="features">
-        <div className="feature-item">
+
+    
+      <div className="praying-hand" id="praying-hand">
+        <div className="praying">
           <h2>Duas and How to do your Tawwaf!</h2>
           <img src="praying-hand.png" alt="Duas" />
         </div>
-        <div className="feature-item">
-          <h2>Guidance on where to go</h2>
-          <img src="guide-panflip.png" alt="Guidance" />
-        </div>
-        <div className="feature-item">
+
+        <div className="pin-point">
           <h2>Real-time location to Masjids near you and Groups!</h2>
           <img src="pin-point.png" alt="RealTimeLocation" />
         </div>
       </div>
+
       <div className="login">
         <p>Already Signed Up?</p>
         <button className="login-button">Login</button>
