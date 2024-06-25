231c59d8e740a25010b601c9e85df5870c14dabc
Ali <alimohamed4813@gmail.com>
2024-06-24 20:38:07 -0400

    Added nav-bar

--- frontend/src/pages/Start Screen/LandingPage.js
+++ frontend/src/pages/Start Screen/LandingPage.js
@@ -4,23 +4,33 @@ import './LandingPage.css';
 const LandingPage = () => {
   return (
     <div className="landing-page">
-      <div className="header">
+      <nav className="navbar">
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#features">Features</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#contact">Contact</a></li>
+          <li><a href="#login">Login</a></li>
+          <li><a href="#signup">Signup</a></li>
+        </ul>
+      </nav>
+      <div className="header" id="home">
         <img src="muslim-pilgrimage.png" alt="Kaaba" className="kaaba-image" />
         <h1>Pilgrimage</h1>
         <p>Community-Verified Umrah Planning with Real-Time Guidance</p>
         <button className="sign-up-button">Sign Up Here!</button>
       </div>
-      <div className="features">
+      <div className="features" id="features">
         <div className="feature-item">
           <h2>Duas and How to do your Tawwaf!</h2>
           <img src="praying-hands.png" alt="Duas" />
         </div>
-        <div className="Guidances">
+        <div className="feature-item">
           <h2>Guidance on where to go</h2>
           <img src="path_to_guidance_image" alt="Guidance" />
         </div>
         <div className="feature-item">
-          <h2>Real time location to Masjids near you and Groups!</h2>
+          <h2>Real-time location to Masjids near you and Groups!</h2>
           <img src="pin-point.png" alt="RealTimeLocation" />
         </div>
       </div>
